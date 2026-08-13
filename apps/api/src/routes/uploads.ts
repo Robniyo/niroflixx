a990868131eac3ea4d4044a28b444bb343653939
robniyo <robertniyonkuru001@gmail.com>
2026-08-13 14:09:23 +0200

    Make upload endpoint public

--- apps/api/src/routes/uploads.ts
+++ apps/api/src/routes/uploads.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import multer from 'multer';
-import { authenticate } from '../middlewares/auth';
 import { uploadsController } from '../controllers/uploads';
 
 const upload = multer({ storage: multer.memoryStorage() });
 
 const router = Router();
-router.post('/', authenticate, upload.single('file'), uploadsController.upload);
+router.post('/', upload.single('file'), uploadsController.upload);
 export default router;
